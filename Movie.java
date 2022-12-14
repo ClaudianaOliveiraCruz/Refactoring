f8a98d47a1e1c2109c0adb7a585936772050abc9
Claudiana Cruz <42771666+ClaudianaOliveiraCruz@users.noreply.github.com>
2022-12-14 11:29:41 -0300

    Commit 12 Movie.java

--- Movie.java
+++ Movie.java
@@ -2,19 +2,17 @@ public class Movie {
   public static final int  CHILDRENS = 2;
   public static final int  REGULAR = 0;
   public static final int  NEW_RELEASE = 1;
-
   private String _title;
   private Price _price;
-
-public Movie(String name, int priceCode) {
+   public Movie(String name, int priceCode) {
       _title = name;
       setPriceCode(priceCode);
    }
-
+    
    public int getPriceCode() {
       return _price.getPriceCode();
    }
-
+   
    public void setPriceCode(int arg) {
       switch (arg) {
          case REGULAR:
@@ -30,34 +28,16 @@ public Movie(String name, int priceCode) {
             throw new IllegalArgumentException("Incorrect Price Code");
       }
    }
-
-public String getTitle (){
+  public String getTitle (){
       return _title;
   }
-   public double getCharge(int daysRented){
-       //Adicionar o trecho de código extraído.
-         double thisAmount = 0;
-      //Adicionar o trecho de código extraído.
 
-switch (getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (daysRented > 2)
-                 thisAmount += (daysRented - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += daysRented * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (daysRented > 3)
-                 thisAmount += (daysRented - 3) * 1.5;
-               break;
-        }
-    return thisAmount;
+   public double getCharge(int daysRented){
+
+  return _price.getCharge(daysRented);
    }
 
-public int getFrequentRenterPoints(int daysRented){
+  public int getFrequentRenterPoints(int daysRented){
        //Adicionar o trecho de código extraído.
     //Adicionar o trecho de código extraído.
     int frequentRenterPoints = 0;
